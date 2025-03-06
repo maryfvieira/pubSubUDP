e0ca7b94dc8a17af3fc8a15babc823baad5b9f33
Mary Felvie <mary.feliciano@gmail.com>
2025-03-05 23:04:06 -0300

    calling start process at Executor

--- sbe-quotation-subscriberWithSchema/src/main/java/org/example/sbe/quotation/subscriber/BasicModule.java
+++ sbe-quotation-subscriberWithSchema/src/main/java/org/example/sbe/quotation/subscriber/BasicModule.java
@@ -6,7 +6,7 @@ public class BasicModule extends AbstractModule {
 
     @Override
     protected void configure() {
-
+        bind(Subscriber.class);
     }
 
 
--- sbe-quotation-subscriberWithSchema/src/main/java/org/example/sbe/quotation/subscriber/Executor.java
+++ sbe-quotation-subscriberWithSchema/src/main/java/org/example/sbe/quotation/subscriber/Executor.java
@@ -1,7 +1,12 @@
 package org.example.sbe.quotation.subscriber;
 
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
 public class Executor {
     public static void main(String[] args) {
-
+        Injector injector = Guice.createInjector(new BasicModule());
+        Subscriber sub = injector.getInstance(Subscriber.class);
+        sub.start();
     }
 }
